c2cc20f98e2ab63dc6cf8729a94a21b04020761a
danielmooklee <daniellee@Daniels-MacBook-Pro-3.local>
2015-05-23 22:27:24 -0500

    Totes added

---
+++ Tetout copy/Assets/floor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floor : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+	/*
+	public void OnTriggerEnter(Collider other) {
+		GameObject g = other.gameObject;
+		/*
+		Transform t = g.transform;
+		if (t.parent != null) {
+			Transform t2 = t.parent;
+			if(t2.childCount <= 1) {
+				GameObject g2 = t2.gameObject;
+				Destroy (g2);
+			}
+		}
+		Destroy(g);
+		* /
+		if (g.name == "ball") {
+			//game over, skrub
+			//Destroy (g);
+
+		}
+	}
+	*/
+}
---
+++ Tetout copy/BreakOut2/Assets/bricks.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bricks : MonoBehaviour {
+
+	private bool stopped;
+	private float speed = 0.03F;
+
+	public GameObject blockPrefab;
+
+	// Use this for initialization
+	public void Start () {
+		//populate with bricks
+		int yOff = 10;
+		for (int x = -10; x <= 10; x++) {
+			for(int y = 0; y < 3; y++) {
+				GameObject block = Instantiate (blockPrefab, new Vector3(x, y + yOff, 0), Quaternion.identity) as GameObject;
+				block.transform.parent = transform;
+				Rigidbody rb = block.rigidbody;
+				rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+			}
+		}
+		stopped = false;
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		if (!stopped) {
+			Vector3 pos = transform.position;
+			float y = pos.y - speed;
+			transform.position = new Vector3 (pos.x, y, 0);
+		}
+	}
+	
+	public void OnCollisionEnter(Collision c) {
+		GameObject o = c.gameObject;
+		Debug.Log ("IN" + o.name);
+		if (o.name == "piece floor") {
+			stopped = true;
+		} else if (o.name == "blockPrefab(Clone)") {
+			o.transform.parent = transform;
+		}
+	}
+
+	public void OnCollisionExit(Collision c) {
+		GameObject o = c.gameObject;
+		Debug.Log ("OUT" + o.name);
+		if (o.name == "piece floor") {
+			stopped = false;
+		}
+	}
+}
