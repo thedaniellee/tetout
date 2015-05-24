02133a82aa0ea8ef537237bd53d9e7858bacff6d
danielmooklee <daniellee@Daniels-MacBook-Pro-3.local>
2015-05-24 00:05:00 -0500

    asdf
    sadf

---
+++ Tetout copy/BreakOut2/Assets/piece.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class piece : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		Vector3 pos = transform.position;
+		float y = pos.y - 0.1F;
+		transform.position = new Vector3 (pos.x, y, 0);
+		if (y < -25) {
+			Destroy (gameObject);
+		}
+	}
+}
