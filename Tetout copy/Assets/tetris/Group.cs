68e2fdea2390950f4c284393559eb938dc077e6d
danielmooklee <daniellee@Daniels-MacBook-Pro-3.local>
2015-05-24 00:29:43 -0500

    asd
    sad

---
+++ Tetout copy/Assets/tetris/Group.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Group : MonoBehaviour {
+
+	float speed = 0.1F;
+
+	bool isValidGridPos() {        
+		foreach (Transform child in transform) {
+			Vector2 v = Grid.roundVec2(child.position);
+			
+			// Not inside Border?
+			if (!Grid.insideBorder(v))
+				return false;
+			
+			// Block in grid cell (and not part of same group)?
+			if (Grid.grid[(int)v.x + Grid.off, (int)v.y] != null &&
+			    Grid.grid[(int)v.x + Grid.off, (int)v.y].parent != transform)
+				return false;
+		}
+		return true;
+	}
+
+	void updateGrid() {
+		// Remove old children from grid
+		for (int y = 0; y < Grid.h; y++)
+			for (int x = 0; x < Grid.w; x++)
+				if (Grid.grid[x, y] != null)
+					if (Grid.grid[x, y].parent == transform)
+						Grid.grid[x, y] = null;
+		
+		// Add new children to grid
+		foreach (Transform child in transform) {
+			Vector2 v = Grid.roundVec2(child.position);
+			Grid.grid[(int)v.x + Grid.off, (int)v.y] = child;
+		}        
+	}
+
+	void Update() {
+		transform.position += new Vector3(0, -speed, 0);
+		
+		// See if valid
+		if (isValidGridPos()) {
+			// It's valid. Update grid.
+			updateGrid();
+		} else {
+			// It's not valid. revert.
+			transform.position += new Vector3(0, speed, 0);
+			// Spawn next Group
+			FindObjectOfType<Spawner>().spawnNext();
+			// Disable script
+			enabled = false;
+		}
+	}
+}
---
+++ Tetout copy/BreakOut2/Assets/controller.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+	
+	public GameObject blockPrefab;
+	public GameObject piecePrefab;
+
+	float prevTime;
+
+	Vector2[][] types;
+
+	// Use this for initialization
+	public void Start () {
+		prevTime = Time.time;
+		types = new Vector2[][] {
+			new Vector2[]{
+				//square
+				new Vector2(0, 0),
+				new Vector2(1, 0),
+				new Vector2(0, 1),
+				new Vector2(1, 1)
+			},
+			new Vector2[]{
+				//line
+				new Vector2(0, 0),
+				new Vector2(0, 1),
+				new Vector2(0, 2),
+				new Vector2(0, 3)
+			},
+			new Vector2[]{
+				//line
+				new Vector2(0, 0),
+				new Vector2(1, -1),
+				new Vector2(1, 0),
+				new Vector2(1, 1)
+			},
+			new Vector2[]{
+				//line
+				new Vector2(0, 0),
+				new Vector2(0, 1),
+				new Vector2(1, 1),
+				new Vector2(1, 2)
+			},
+			new Vector2[]{
+				//line
+				new Vector2(0, 0),
+				new Vector2(0, -1),
+				new Vector2(1, -1),
+				new Vector2(1, -2)
+			},
+			new Vector2[]{
+				//line
+				new Vector2(0, 0),
+				new Vector2(0, 1),
+				new Vector2(1, 1),
+				new Vector2(2, 1)
+			},
+			new Vector2[]{
+				//line
+				new Vector2(0, 0),
+				new Vector2(0, -1),
+				new Vector2(1, -1),
+				new Vector2(2, -1)
+			}
+		};
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		if (Time.time - prevTime > 1F) {
+			int xOff = Random.Range(-5, 6);
+			int yOff = 15;
+			GameObject piece = Instantiate (piecePrefab, new Vector3 (xOff, yOff, 0), Quaternion.identity) as GameObject;
+			Vector2[] v = types[Random.Range(0, types.Length)];
+			for (int i = 0; i < v.Length; i++) {
+				//int switcher = Random.Range (0, 3);
+				Vector2 v2 = v[i];
+				GameObject block = Instantiate (blockPrefab, new Vector3(v2.x + xOff, v2.y + yOff, 0), Quaternion.identity) as GameObject;
+				block.transform.SetParent (piece.transform);
+				Rigidbody rb = block.rigidbody;
+				rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+			}
+			piece.AddComponent("piece");
+			prevTime = Time.time;
+		}
+	}
+}
